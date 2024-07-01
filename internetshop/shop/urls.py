0dbe40358d10bbdd37edaf1985e5172720322e24
GSkorpik <gosha.midural@gmail.com>
2024-07-01 18:39:44 +0500

    ///

--- internetshop/shop/urls.py
+++ internetshop/shop/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("product", views.view_product, name="view_product")
 ]
--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -1,14 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Product
-
 # Create your views here.
 def home(request):
-    products = Product.objects.all()
-    return render(request, "index.html", {
-        'products': products
-    })
-
-def view_product(request):
-    return render(request, 'product.html')
+    return render(request, "index.html")
